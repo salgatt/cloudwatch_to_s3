90ccae642832c54ce14cbe312019117eed477aa2
Scott <salgatt@turtleshell.net>
2022-10-31 17:00:18 -0400

    Code all fixed up with precommits

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-from modules import cloudwatch_to_s3
 import logging
 import os
+
+from modules import cloudwatch_to_s3
 
 """
 This portion will obtain the Environment variables
 """
-GROUP_NAME = os.environ['GROUP_NAME']
-DESTINATION_BUCKET = os.environ['DESTINATION_BUCKET']
-PREFIX = os.environ['PREFIX']
-NDAYS = os.environ['NDAYS']
+GROUP_NAME = os.environ["GROUP_NAME"]
+DESTINATION_BUCKET = os.environ["DESTINATION_BUCKET"]
+PREFIX = os.environ["PREFIX"]
+NDAYS = os.environ["NDAYS"]
 n_days = int(NDAYS)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        response = cloudwatch_to_s3.run(group_name = GROUP_NAME, s3_bucket_name = DESTINATION_BUCKET, s3_bucket_prefix = PREFIX, n_days = n_days)
+        response = cloudwatch_to_s3.run(
+            group_name=GROUP_NAME,
+            s3_bucket_name=DESTINATION_BUCKET,
+            s3_bucket_prefix=PREFIX,
+            n_days=n_days,
+        )
         print(response)
     except Exception as e:
         logging.error(e)
         print(e)
-
--- modules/cloudwatch_to_s3.py
+++ modules/cloudwatch_to_s3.py
@@ -1,8 +1,9 @@
-import boto3
-import os
 import datetime
 import logging
+import os
 import time
+
+import boto3
 
 """
 This portion will receive the n_days value (the date/day of the log you want
@@ -12,18 +13,23 @@ Ex: If today is April 13th and NDAYS = 0, April 13th logs will be exported.
 Ex: If today is April 13th and NDAYS = 1, April 12th logs will be exported.
 Ex: If today is April 13th and NDAYS = 2, April 11th logs will be exported.
 """
-def generate_date_dict(n_days = 1):
+
+
+def generate_date_dict(n_days=1):
     currentTime = datetime.datetime.now()
     date_dict = {
-        "start_date" : currentTime - datetime.timedelta(days=n_days),
-        "end_date" : currentTime - datetime.timedelta(days=n_days - 1),
+        "start_date": currentTime - datetime.timedelta(days=n_days),
+        "end_date": currentTime - datetime.timedelta(days=n_days - 1),
     }
     return date_dict
+
 
 """
 Convert the from & to Dates to milliseconds
 """
-def convert_from_date(date_time = datetime.datetime.now()):
+
+
+def convert_from_date(date_time=datetime.datetime.now()):
     return int(date_time.timestamp() * 1000)
 
 
@@ -31,64 +37,73 @@ def convert_from_date(date_time = datetime.datetime.now()):
 The following will create the subfolders' structure based on year, month, day
 Ex: BucketNAME/LogGroupName/Year/Month/Day
 """
+
+
 def generate_prefix(s3_bucket_prefix, start_date):
-    return os.path.join(s3_bucket_prefix, start_date.strftime('%Y{0}%m{0}%d').format(os.path.sep))
+    return os.path.join(
+        s3_bucket_prefix, start_date.strftime("%Y{0}%m{0}%d").format(os.path.sep)
+    )
 
 
 """
 Based on the AWS boto3 documentation
 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/logs.html#CloudWatchLogs.Client.create_export_task
 """
-def create_export_task(group_name, from_date, to_date, s3_bucket_name, s3_bucket_prefix):
+
+
+def create_export_task(
+    group_name, from_date, to_date, s3_bucket_name, s3_bucket_prefix
+):
     logging.info("Creating CloudWatch Log Export Task to S3")
     try:
-        client = boto3.client('logs')
+        client = boto3.client("logs")
         response = client.create_export_task(
-             logGroupName=group_name,
-             fromTime=from_date,
-             to=to_date,
-             destination=s3_bucket_name,
-             destinationPrefix=s3_bucket_prefix
-            )
-        if 'taskId' not in response:
+            logGroupName=group_name,
+            fromTime=from_date,
+            to=to_date,
+            destination=s3_bucket_name,
+            destinationPrefix=s3_bucket_prefix,
+        )
+        if "taskId" not in response:
             logging.error("Unexpected createExportTask response")
             raise Exception("Unexpected createExportTask response")
-        return response['taskId']
+        return response["taskId"]
     except Exception as e:
         logging.error(e)
         raise Exception("Failed to Create Export Task")
+
 
 """
 Based on the AWS boto3 documentation
 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/logs.html#CloudWatchLogs.Client.describe_export_tasks
 """
+
+
 def export_status_call(task_id):
     try:
-        client = boto3.client('logs')
-        response = client.describe_export_tasks(
-             taskId = task_id
-            )
+        client = boto3.client("logs")
+        response = client.describe_export_tasks(taskId=task_id)
 
-        if 'exportTasks' not in response:
+        if "exportTasks" not in response:
             logging.error("Unexpected describeExportTasks response")
             raise Exception("Unexpected describeExportTasks response")
 
-        if len(response['exportTasks']) != 1:
+        if len(response["exportTasks"]) != 1:
             logging.error("Wrong number of export tasks found")
             raise Exception("Wrong number of export tasks found")
 
-        if "status" not in response['exportTasks'][0]:
+        if "status" not in response["exportTasks"][0]:
             logging.error("No status found in describeExportTasks response")
             raise Exception("No status found in describeExportTasks response")
 
-        if "code" not in response['exportTasks'][0]['status']:
+        if "code" not in response["exportTasks"][0]["status"]:
             logging.error("No status code found in describeExportTasks response")
             raise Exception("No status code found in describeExportTasks response")
 
-        if response['exportTasks'][0]['status']['code'] == 'RUNNING':
+        if response["exportTasks"][0]["status"]["code"] == "RUNNING":
             return "RUNNING"
 
-        if response['exportTasks'][0]['status']['code'] == 'COMPLETED':
+        if response["exportTasks"][0]["status"]["code"] == "COMPLETED":
             return "COMPLETED"
 
         # Otherwise we should be in a failed state
@@ -99,16 +114,22 @@ def export_status_call(task_id):
         logging.error(e)
         raise Exception("Status Call Failed")
 
-def check_export_status(task_id, delay = 1):
+
+def check_export_status(task_id, delay=1):
     logging.info("Checking Status of Export Task")
     try:
-        response = export_status_call(task_id = task_id)
+        response = export_status_call(task_id=task_id)
 
-        # We want to be sure we're not beating up the API and we'll increase our delay until the task completes
-        while(response == 'RUNNING'):
-            logging.warning("We did not get a COMPLETED response so waiting " + str(delay) + " seconds until checking status again")
+        # We want to be sure we're not beating up the API and
+        # we'll increase our delay until the task completes
+        while response == "RUNNING":
+            logging.warning(
+                "We did not get a COMPLETED response so waiting "
+                + str(delay)
+                + " seconds until checking status again"
+            )
             time.sleep(delay)
-            response = export_status_call(task_id = task_id)
+            response = export_status_call(task_id=task_id)
             delay = delay * 2
 
         logging.info("Log Export Task has completed")
@@ -118,26 +139,36 @@ def check_export_status(task_id, delay = 1):
         logging.error(e)
         raise Exception("Describe Check Failed")
 
+
 """
 The main function in here
 """
+
+
 def run(group_name, s3_bucket_name, s3_bucket_prefix, n_days):
     try:
         """
-        Based upon what we've been provided for n_days, we'll generate the dates needed to run
+        Based upon what we've been provided for n_days,
+        we'll generate the dates needed to run
         """
-        date_dict = generate_date_dict(n_days = n_days)
-        date_dict['start_date_ms'] = convert_from_date(date_time = date_dict['start_date'])
-        date_dict['end_date_ms'] = convert_from_date(date_time = date_dict['end_date'])
-        s3_bucket_prefix_with_date = generate_prefix(s3_bucket_prefix = s3_bucket_prefix, start_date = date_dict['start_date'])
-        export_task_id = create_export_task(group_name = group_name, from_date = date_dict['start_date_ms'], to_date = date_dict['end_date_ms'], s3_bucket_name = s3_bucket_name, s3_bucket_prefix = s3_bucket_prefix_with_date)
+        date_dict = generate_date_dict(n_days=n_days)
+        date_dict["start_date_ms"] = convert_from_date(
+            date_time=date_dict["start_date"]
+        )
+        date_dict["end_date_ms"] = convert_from_date(date_time=date_dict["end_date"])
+        s3_bucket_prefix_with_date = generate_prefix(
+            s3_bucket_prefix=s3_bucket_prefix, start_date=date_dict["start_date"]
+        )
+        export_task_id = create_export_task(
+            group_name=group_name,
+            from_date=date_dict["start_date_ms"],
+            to_date=date_dict["end_date_ms"],
+            s3_bucket_name=s3_bucket_name,
+            s3_bucket_prefix=s3_bucket_prefix_with_date,
+        )
         logging.debug("Export Task ID : " + export_task_id)
-        check_export_status(task_id = export_task_id)
-        return {
-          "task_id" : export_task_id,
-          "s3_export_path" : s3_bucket_prefix_with_date
-        }
+        check_export_status(task_id=export_task_id)
+        return {"task_id": export_task_id, "s3_export_path": s3_bucket_prefix_with_date}
     except Exception as e:
         logging.error(e)
         raise e
-
